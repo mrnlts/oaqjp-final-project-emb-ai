69524ad60015fd054fcd6d137e4087f85c834a83
Marina <mrnlts1@gmail.com>
2026-03-17 14:44:46 -0400

    Adds basic endpoints for server

--- final_project/server.py
+++ final_project/server.py
@@ -1,1 +1,16 @@
-from flask import flask
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("My Emotion Detector")
+
+@app.route('/')
+def home():
+    if request.method == 'GET':
+        return render_template('index.html')
+
+@app.route('/emotionDetector')
+def emotionDetector():
+    response = emotion_detector(request.args.get('textToAnalyze')) 
+    return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+
+app.run(host="0.0.0.0", port=5000)
